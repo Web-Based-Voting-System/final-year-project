560c59183173bcfe0c67124d795f0ce693628255
Frank Kwabena Abrokwa <76174290+Kwabena-Abrokwa@users.noreply.github.com>
2022-04-24 18:21:09 +0000

    Setup

---
+++ src/Pages/Auth/VotingPageConfirmation.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const VotingPageConfirmation = () => {
+  return (
+    <div>VotingPageConfirmation</div>
+  )
+}
+
+export default VotingPageConfirmation
---
+++ src/Pages/Auth/VotingResultsPerCandidate.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const VotingResultsPerCandidate = () => {
+  return (
+    <div>VotingResultsPerCandidate</div>
+  )
+}
+
+export default VotingResultsPerCandidate
